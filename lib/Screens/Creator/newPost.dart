8b9a30cf2e755503f700aed17a82f1f8e5edb5b2
abhisharma1601 <abhi12345950@gmail.com>
2023-04-22 17:30:49 +0530

    new_note

--- lib/Screens/Creator/newPost.dart
+++ lib/Screens/Creator/newPost.dart
@@ -1,6 +1,9 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:get/get.dart';
+import 'package:notio/main.dart';
 import 'package:notio/utility.dart';
 
 class newPost extends StatefulWidget {
@@ -13,21 +16,44 @@ class newPost extends StatefulWidget {
 class _newPostState extends State<newPost> {
   List<Widget> _tags = [];
   String _tag = "";
+  String _subject = "Select Subject";
+
+  List<String> _subjects = ["Select Subject", "VLSI", "DSA"];
+
+  int _sem = 4;
+
+  bool _visible = false;
+
+  List<int> _sems = new List<int>.generate(currentUser.sem, (i) => i + 1);
+
+  Future<void> pickFile() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles();
+
+   
+
+    
+
+    if (result != null) {
+      PlatformFile file = result.files.first;
+      print('Picked file: ${file.name}');
+    } else {}
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 0,
+        backgroundColor: Colors.blue, // Status bar color
+      ),
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.only(
-          left: getwidth(context, 30),
-          top: getheight(context, 58),
-          right: getwidth(context, 30),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: EdgeInsets.fromLTRB(getwidth(context, 10),
+                getheight(context, 20), getwidth(context, 10), 0),
+            child: Row(
               children: [
                 GestureDetector(
                   onTap: () {
@@ -43,15 +69,19 @@ class _newPostState extends State<newPost> {
                   "New Contribution",
                   style: TextStyle(
                       color: Colors.black,
-                      fontSize: 24,
+                      fontSize: 22,
                       fontWeight: FontWeight.w700),
                 )
               ],
             ),
-            SizedBox(
-              height: getheight(context, 44),
-            ),
-            TextField(
+          ),
+          SizedBox(
+            height: getheight(context, 30),
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB(
+                getwidth(context, 20), 0, getwidth(context, 20), 0),
+            child: TextField(
               decoration: InputDecoration(
                   icon: Icon(Icons.copy),
                   hintText: "Title for you Notes.",
@@ -60,10 +90,14 @@ class _newPostState extends State<newPost> {
                       fontSize: 18,
                       fontWeight: FontWeight.w800)),
             ),
-            SizedBox(
-              height: getheight(context, 16),
-            ),
-            TextField(
+          ),
+          SizedBox(
+            height: getheight(context, 16),
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB(
+                getwidth(context, 20), 0, getwidth(context, 20), 0),
+            child: TextField(
               decoration: InputDecoration(
                   icon: Icon(Icons.description),
                   hintText: "One line discription.",
@@ -72,66 +106,158 @@ class _newPostState extends State<newPost> {
                       fontSize: 16,
                       fontWeight: FontWeight.w500)),
             ),
-            SizedBox(
-              height: getheight(context, 35),
+          ),
+          SizedBox(
+            height: getheight(context, 16),
+          ),
+
+          //area 51
+
+          Container(
+            margin: EdgeInsets.fromLTRB(
+                getwidth(context, 20), 0, getwidth(context, 20), 0),
+            decoration: BoxDecoration(
+              border: Border(
+                bottom: BorderSide(color: Colors.grey, width: 1.0),
+              ),
             ),
-            GestureDetector(
-              onTap: () {
-                showDialog(
-                  context: context,
-                  builder: (BuildContext context) => Dialog(
-                    child: Container(
-                      height: getheight(context, 100),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Container(
-                            width: getwidth(context, 200),
-                            child: TextField(
-                              onChanged: (value) {
-                                _tag = value;
-                              },
-                              decoration: InputDecoration(
-                                  hintText: "Enter Tag",
-                                  hintStyle: TextStyle(
-                                      color: blueColor,
-                                      fontWeight: FontWeight.w800)),
-                            ),
-                          ),
-                          InkWell(
-                            onTap: () {
-                              setState(() {
-                                if (_tag != "") {
-                                  _tags.add(
-                                    _tagChip(
-                                        header: _tag,
-                                        reload: (String chip) {
-                                          print(_tags);
-                                          for (var element in _tags) {
-                                            print(element);
-                                          }
-                                          setState(() {});
-                                        }),
-                                  );
-                                  _tag = "";
-                                }
-                              });
+            child: DropdownButtonFormField<int>(
+              menuMaxHeight: 260,
+              value: _sems[0],
+              decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.leaderboard),
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25),
+                      borderSide: BorderSide.none),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25),
+                      borderSide: BorderSide.none),
+                  // filled: true,
+                  fillColor: Colors.white),
+              icon: const Icon(Icons.keyboard_arrow_down),
+              elevation: 16,
+              style: const TextStyle(
+                  color: Colors.black, fontWeight: FontWeight.normal),
+              onChanged: (newValue) {
+                _sem = newValue!;
+                setState(() {
+                  _visible = true;
+                });
+              },
+              items: _sems.map<DropdownMenuItem<int>>((value) {
+                return DropdownMenuItem<int>(
+                  value: value,
+                  child: Text(value.toString()),
+                );
+              }).toList(),
+            ),
+          ),
+
+          Visibility(
+            visible: _visible,
+            child: Container(
+              margin: EdgeInsets.fromLTRB(
+                  getwidth(context, 20), 0, getwidth(context, 20), 0),
+              decoration: BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(color: Colors.grey, width: 1.0),
+                ),
+              ),
+              child: DropdownButtonFormField<String>(
+                menuMaxHeight: 260,
+                value: _subject,
+                decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.subject),
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25),
+                        borderSide: BorderSide.none),
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25),
+                        borderSide: BorderSide.none),
+                    // filled: true,
+                    fillColor: Colors.white),
+                icon: const Icon(Icons.keyboard_arrow_down),
+                elevation: 16,
+                style: const TextStyle(
+                    color: Colors.black, fontWeight: FontWeight.normal),
+                onChanged: (newValue) {
+                  _subject = newValue!;
+                },
+                items: _subjects.map<DropdownMenuItem<String>>((value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              ),
+            ),
+          ),
+
+          //area 51
+
+          SizedBox(
+            height: getheight(context, 35),
+          ),
+
+          GestureDetector(
+            onTap: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) => Dialog(
+                  child: Container(
+                    height: getheight(context, 100),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                          width: getwidth(context, 200),
+                          child: TextField(
+                            onChanged: (value) {
+                              _tag = value;
                             },
-                            child: Icon(
-                              Icons.add,
-                              color: blueColor,
-                            ),
-                          )
-                        ],
-                      ),
+                            decoration: InputDecoration(
+                                hintText: "Enter Tag",
+                                hintStyle: TextStyle(
+                                    color: blueColor,
+                                    fontWeight: FontWeight.w800)),
+                          ),
+                        ),
+                        InkWell(
+                          onTap: () {
+                            setState(() {
+                              if (_tag != "") {
+                                _tags.add(
+                                  _tagChip(
+                                      header: _tag,
+                                      reload: (String chip) {
+                                        print(_tags);
+                                        for (var element in _tags) {
+                                          print(element);
+                                        }
+                                        setState(() {
+                                          _tag = "";
+                                        });
+                                      }),
+                                );
+                              }
+                            });
+                          },
+                          child: Icon(
+                            Icons.add,
+                            color: blueColor,
+                          ),
+                        )
+                      ],
                     ),
                   ),
-                );
-              },
+                ),
+              );
+            },
+            child: Center(
               child: Text(
                 "Add Tags",
                 style: TextStyle(
@@ -140,18 +266,27 @@ class _newPostState extends State<newPost> {
                     fontSize: 14),
               ),
             ),
-            SizedBox(
-              height: getheight(context, 14),
-            ),
-            Wrap(
+          ),
+          SizedBox(
+            height: getheight(context, 14),
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB(
+                getwidth(context, 20), 0, getwidth(context, 20), 0),
+            child: Wrap(
               children: _tags,
               spacing: 6,
               runAlignment: WrapAlignment.start,
             ),
-            SizedBox(
-              height: getheight(context, 115),
-            ),
-            Center(
+          ),
+          SizedBox(
+            height: getheight(context, 115),
+          ),
+          Center(
+            child: GestureDetector(
+              onTap: () {
+                pickFile();
+              },
               child: Container(
                 padding: EdgeInsets.symmetric(vertical: 10),
                 width: getwidth(context, 190),
@@ -185,71 +320,35 @@ class _newPostState extends State<newPost> {
                 ),
               ),
             ),
-            SizedBox(
-              height: getheight(context, 20),
-            ),
-            Center(
+          ),
+          //
+          Spacer(),
+          GestureDetector(
+            onTap: () {
+              ////////// SUBMIT
+            },
+            child: Center(
               child: Container(
-                padding: EdgeInsets.symmetric(vertical: 10),
-                width: getwidth(context, 190),
+                padding: EdgeInsets.symmetric(vertical: 14),
+                width: getwidth(context, 212),
                 decoration: BoxDecoration(
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.5),
-                        spreadRadius: 6,
-                        blurRadius: 8,
-                        offset: Offset(0, 4),
-                      ),
-                    ],
-                    color: Color(0xFF0D253C),
-                    borderRadius: BorderRadius.circular(60)),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(
-                      Icons.image,
-                      color: Colors.white,
-                    ),
-                    SizedBox(
-                      width: getwidth(context, 8),
-                    ),
-                    Text(
-                      "Upload Image",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Spacer(),
-            GestureDetector(
-              onTap: () {
-                ////////// SUBMIT
-              },
-              child: Center(
-                child: Container(
-                  padding: EdgeInsets.symmetric(vertical: 14),
-                  width: getwidth(context, 212),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(60),
-                      color: blueColor),
-                  child: const Center(
-                    child: Text(
-                      "PUBLISH",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16),
-                    ),
+                    borderRadius: BorderRadius.circular(60), color: blueColor),
+                child: const Center(
+                  child: Text(
+                    "PUBLISH",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16),
                   ),
                 ),
               ),
             ),
-            SizedBox(
-              height: getheight(context, 35),
-            )
-          ],
-        ),
+          ),
+          SizedBox(
+            height: getheight(context, 35),
+          )
+        ],
       ),
     );
   }
