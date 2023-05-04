43ab7fa35b73dfa3fb8884745ae8c670ff24ce8b
abhisharma1601 <abhi12345950@gmail.com>
2023-05-04 23:46:02 +0530

    g

--- lib/apiServices/storyModuleServices.dart
+++ lib/apiServices/storyModuleServices.dart
@@ -1,21 +1,16 @@
 import 'dart:convert';
 import 'dart:io';
-
-import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
-import 'package:http/http.dart';
 import 'package:http_parser/http_parser.dart';
+import 'package:notio/keys.dart';
 
 class storyServices {
-  // String _api_url =
-  //     "http://ec2-43-204-30-204.ap-south-1.compute.amazonaws.com:8080";
-
-  String _api_url = "http://192.168.1.2:8080";
+  
 
   uploadStory(File file, Object data) async {
     http.MultipartRequest req = http.MultipartRequest(
       'POST',
-      Uri.parse("$_api_url/userStory/addStory"),
+      Uri.parse("$api_url/userStory/addStory"),
     );
 
     req.files.add(await http.MultipartFile.fromPath("image", file.path));
@@ -27,7 +22,7 @@ class storyServices {
   }
 
   getStroiesapi(Object body) async {
-    return await http.post(Uri.parse("$_api_url/userStory/getStories"),
+    return await http.post(Uri.parse("$api_url/userStory/getStories"),
         headers: <String, String>{
           'Content-Type': 'application/json',
         },
@@ -35,7 +30,7 @@ class storyServices {
   }
 
   addBolt(Object body) async {
-    return await http.post(Uri.parse("$_api_url/userStory/addBolt"),
+    return await http.post(Uri.parse("$api_url/userStory/addBolt"),
         headers: <String, String>{
           'Content-Type': 'application/json',
         },
@@ -43,7 +38,7 @@ class storyServices {
   }
 
   addView(Object body) async {
-    return await http.post(Uri.parse("$_api_url/userStory/addView"),
+    return await http.post(Uri.parse("$api_url/userStory/addView"),
         headers: <String, String>{
           'Content-Type': 'application/json',
         },
